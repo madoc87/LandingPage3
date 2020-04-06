d78287d94580692a601b8d36e402b4d294a49f8d
Fabricio Assis <fabricio.r.assis@gmail.com>
2020-04-06 19:28:07 -0300

    Add JS
    adicionado JS para carregar o mapa após carregamento da pagina

---
+++ js/functions.js
@@ -0,0 +1,2 @@
+
+window.onload = function(){document.getElementById('map').innerHTML='<iframe id="gmap_canvas" src="https://maps.google.com/maps?q=cowmeia&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';}
